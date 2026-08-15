2757766527bbcd27ef1627d8471e6f19e5bb4ed2
XXHieXX <XXHieXX111@gmail.com>
2026-08-15 17:06:27 +0400

    Example fix

--- Examples/Textures.py
+++ Examples/Textures.py
@@ -6,8 +6,8 @@ texture_raw = Renderer.TextureReader(textures.Get("LightGrid"))
 texture = Renderer.TextureLoader(texture_raw, TextureFilter.NEAREST)
 
 rect = gfx.Rectangle()
-Transform.SetPosition(rect, Vec2(0, 0))
-Transform.SetSize(rect, Vec2(512, 512))
+Transform.SetPosition(rect, Vec2(128, 128))
+Transform.SetSize(rect, Vec2(256, 256))
 Color.Set(rect, Color3(1, 1 ,1))
 Texture.Set(rect, texture)
 
